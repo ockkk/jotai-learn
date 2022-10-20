963ea974cc8a9ea659d2048550292daaa331410f
ockkk <jongock.dev@gamil.com>
2022-10-20 23:22:00 +0900

    feat: Header component 추가

---
+++ src/header/Header.tsx
@@ -0,0 +1,15 @@
+export function Header() {
+  return (
+    <div>
+      <h2>Jotai learn</h2>
+      <ul>
+        <li>원시 atom</li>
+        <li>읽기 atom</li>
+        <li>쓰기 atom</li>
+        <li>읽기 쓰기 atom</li>
+        <li>utils atom</li>
+        <li>provider</li>
+      </ul>
+    </div>
+  );
+}
